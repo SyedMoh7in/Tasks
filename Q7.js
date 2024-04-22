a2e7f281043971368403ad349a70f5d36045bb2d
SyedMoh7in <149953029+SyedMoh7in@users.noreply.github.com>
2024-04-23 02:38:38 +0500

    Update Q7.js

--- Q7.js
+++ Q7.js
@@ -2,6 +2,24 @@ function palindrome(input) {
     const ans = input.split('').reverse().join('');
     return input === ans;
 }
+console.log(palindrome("aabaa"));
+console.log(palindrome("abac")); 
+console.log(palindrome("a")); 
+
+//Method 2:
+function palindrome(input) {
+    const ans = input.split('').reverse().join('');
+    {
+if(input==ans)
+{
+    return true;
+}
+else{
+    return false;
+}
+}
+}
+console.log(palindrome("aabaa"));
 console.log(palindrome("abac")); 
 console.log(palindrome("a")); 
 
